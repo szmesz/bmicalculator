2c80b7e8a8cee9310c0a795d8708368c4628ed12
szmesz <vajda.tamas.kk.1@gmail.com>
2017-10-28 21:47:26 +0100

    Add Basic Calculations

--- bmicalculator/src/main/java/hu/epam/App.java
+++ bmicalculator/src/main/java/hu/epam/App.java
@@ -1,13 +1,34 @@
 package hu.epam;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * BMIcalculator
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please pick from the two numbers: 1 or 2!\n"
+				 + "If you would like to use the BMI Calculator on Metric values input 1\n"
+				 + "If you would like to use the BMI Calculator on USC valuse input 2\n"
+				 + "Thank you");
+		int input = scn.nextInt();
+		
+		switch (input) { 
+				
+				case 1: 
+					if (input == 1)
+					System.out.println("You chose the Metric system");
+					new MetricBmiCalculation();
+					break;
+				case 2: 
+					if (input == 2)
+					System.out.println("You chose the USC system");
+					new UscBmiCalculator();
+					break;
+				default:
+					System.out.println("You should pick from 1 or 2...");
+		}
+	}
 }
---
+++ bmicalculator/src/main/java/hu/epam/UscBmiCalculator.java
@@ -0,0 +1,49 @@
+package hu.epam;
+
+import java.util.Scanner;
+
+public class UscBmiCalculator {
+
+	public UscBmiCalculator() {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("Please input your height in inches");
+		float input1 = scn.nextFloat();
+		if (input1 > 0)
+			System.out.println("Your height is " + input1 + " in.");
+		else
+			System.out.println("Please input greater value than 0!");
+
+		System.out.println("Please input your weight in lbs");
+		float input2 = scn.nextFloat();
+		if (input2 > 0)
+			System.out.println("Your weight is " + input2 + " lbs.");
+		else
+			System.out.println("Please input greater value than 0!");
+		float i = (703 * ((input2 / ((input1 / 100) * (input1 / 100)))));
+		System.out.println("Your Body Mass Index is : " + i);
+		
+		
+		if (i<16) {
+			System.out.println("Your BMI Category is: Severe Thinness");
+		} else if (i>=16 && i<=17) {
+			System.out.println("Your BMI Category is: Moderate Thinness");
+		} else if (i>=17 && i<=18.5) {
+			System.out.println("Your BMI Category is: Mild Thinness");
+		} else if (i>=18.5 && i<=25) {
+			System.out.println("Your BMI Category is: Normal");
+		}else if (i>=25 && i<=30) {
+			System.out.println("Your BMI Category is: Overweight");
+		}else if (i>=30 && i<=35) {
+			System.out.println("Your BMI Category is: Obese Class I");
+		}else if(i>=35&&i<=40){
+		System.out.println("Your BMI Category is: Obese CLass II");
+		}else if(i>40){
+		System.out.println("Your BMI Category is: Obese Class III");
+		scn.close();
+		}
+	}
+
+}
+
+
