3c51565ac0b93d35633a57b415e6c05ea9d475f6
szmesz <vajda.tamas.kk.1@gmail.com>
2017-11-01 19:41:30 +0000

    add Tests

--- bmicalculator/src/main/java/hu/bmicalculator2/BMICalculator.java
+++ bmicalculator/src/main/java/hu/bmicalculator2/BMICalculator.java
@@ -3,37 +3,34 @@ package hu.bmicalculator2;
 public class BMICalculator {
 
 	public float calculateBMIIndex(UserAttributes userAttributes) {
-		float BMIIndex = (float) (userAttributes.getWeight() / ((Math.pow(userAttributes.getHeight(), 2)) / 1000));
+		float BMIIndex = (float) (userAttributes.getWeight() / (Math.pow(userAttributes.getHeight(), 2)));
 		return BMIIndex;
 	}
 
 	public String getBMICategory(float BMIIndex) {
 
-		String[] myStringArray;
-		{
-			myStringArray = new String[8];
-			myStringArray = new String[] { "Severe Thinness", "Moderate Thinness", "Mild Thinness", "Normal",
-					"Overweight", "Obese Class I", "Obese CLass II", "Obese Class III" };
-			String BMICategory = "";
-			
-			if (BMIIndex < 16) {
-				BMICategory = myStringArray[0];
-			} else if (BMIIndex >= 16 && BMIIndex <= 17) {
-				BMICategory = myStringArray[1];
-			} else if (BMIIndex >= 17 && BMIIndex <= 18.5) {
-				BMICategory = myStringArray[2];
-			} else if (BMIIndex >= 18.5 && BMIIndex <= 25) {
-				BMICategory = myStringArray[3];
-			} else if (BMIIndex >= 25 && BMIIndex <= 30) {
-				BMICategory = myStringArray[4];
-			} else if (BMIIndex >= 30 && BMIIndex <= 35) {
-				BMICategory = myStringArray[5];
-			} else if (BMIIndex >= 35 && BMIIndex <= 40) {
-				BMICategory = myStringArray[6];
-			} else if (BMIIndex > 40) {
-				BMICategory = myStringArray[7];
-			}
-			return BMICategory;
+		String[] myStringArray = new String[] { "Severe Thinness", "Moderate Thinness", "Mild Thinness", "Normal",
+				"Overweight", "Obese Class I", "Obese CLass II", "Obese Class III" };
+		String BMICategory = "";
+
+		if (BMIIndex < 16) {
+			BMICategory = myStringArray[0];
+		} else if (BMIIndex >= 16 && BMIIndex <= 17) {
+			BMICategory = myStringArray[1];
+		} else if (BMIIndex >= 17 && BMIIndex <= 18.5) {
+			BMICategory = myStringArray[2];
+		} else if (BMIIndex >= 18.5 && BMIIndex <= 25) {
+			BMICategory = myStringArray[3];
+		} else if (BMIIndex >= 25 && BMIIndex <= 30) {
+			BMICategory = myStringArray[4];
+		} else if (BMIIndex >= 30 && BMIIndex <= 35) {
+			BMICategory = myStringArray[5];
+		} else if (BMIIndex >= 35 && BMIIndex <= 40) {
+			BMICategory = myStringArray[6];
+		} else if (BMIIndex > 40) {
+			BMICategory = myStringArray[7];
 		}
+		return BMICategory;
+
 	}
 }
--- bmicalculator/src/main/java/hu/bmicalculator2/UserInput.java
+++ bmicalculator/src/main/java/hu/bmicalculator2/UserInput.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UserInput {
-	private UserAttributes userAttributes;
-
 	public UserInput() {
 	}
 
@@ -13,36 +11,42 @@ public class UserInput {
 		Scanner scn = new Scanner(System.in);
 
 		String heightUnit = readUnitFromConsole(scn, "height", HeightUnit.getHeightValues());
-		scn.nextLine();
+		// scn.nextLine();
 		float height = readAttributeFromConsole(scn, "height");
 		System.out.println("Your height is:" + height + heightUnit);
-		if (heightUnit.toLowerCase().equals("inch")) {
-			height = (height * 2.54f);
-			scn.close();
+		if (heightUnit.toLowerCase().equals("cm")) {
+			height = (height / 100f);
+		} else {
+			height = (height * 0.0254f);
 		}
+
 		scn.nextLine();
 		String weightUnit = readUnitFromConsole(scn, "weight", WeightUnit.getWeightValues());
-		scn.nextLine();
+		// scn.nextLine();
 		float weight = readAttributeFromConsole(scn, "weight");
 		System.out.println("Your weight is:" + weight + weightUnit);
 		if (weightUnit.toLowerCase().equals("lbs")) {
 			weight = weight * 0.45359237f;
-			scn.close();
 		}
 
-		UserAttributes userAttributes = new UserAttributes(weight, height, weightUnit, heightUnit);
+		UserAttributes userAttributes = new UserAttributes(height, weight, heightUnit, weightUnit);
+		scn.close();
 		return userAttributes;
+
 	}
 
 	private String readUnitFromConsole(Scanner scn, String attribute, List<String> validValues) {
 		String unit = "";
-		System.out.println("Please type the unit of " + attribute + "!");
+		do {
 
-		unit = scn.nextLine();
-		if (!validValues.contains(unit.toUpperCase())) {
-			System.out.println("Please enter a valid unit!");
-			scn.close();
-		}
+			System.out.println("Please type the unit of " + attribute + "!");
+			System.out.println("Valid values are:");
+			for (String actualValue : validValues) {
+				System.out.println(actualValue);
+			}
+			unit = scn.nextLine();
+
+		} while (!validValues.contains(unit.toUpperCase()));
 		return unit;
 
 	}
@@ -51,16 +55,19 @@ public class UserInput {
 		float attributeValue = 0f;
 
 		System.out.println("Please type your " + attribute + "!");
-		try {
-			attributeValue = scn.nextFloat();
-			if (attributeValue < 0)
-				throw new IllegalArgumentException();
-		} catch (Exception exception) {
-			System.out.println("Please enter a valid value!");
-			scn.close();
+		while (true) {
+			try {
+
+				attributeValue = scn.nextFloat();
+				if (attributeValue < 0)
+					throw new IllegalArgumentException();
+				return attributeValue;
+			} catch (Exception exception) {
+				System.out.println("Please enter a valid value!");
+				scn.next();
+			}
 		}
 
-		return attributeValue;
 	}
 
 }
